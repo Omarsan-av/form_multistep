d98d0a4008a6f81bd9eab5469e67b2cef21408d4
Omar Sánchez Avelar <omar_sanchez_avelar@hotmail.com>
2021-11-11 09:12:45 -0600

    version movil terminada-8

--- js/index.js
+++ js/index.js
@@ -31,57 +31,44 @@ function showTab(n)
 
 function nextPrev(n) 
 {
-   // This function will figure out which tab to display
-   var x = document.getElementsByClassName("tab");
+   let x = document.getElementsByClassName("tab");
 
-   // Exit the function if any field in the current tab is invalid:
    if (n == 1 && !validateForm()) return false;
 
-   // Hide the current tab:
    x[currentTab].style.display = "none";
 
-   // Increase or decrease the current tab by 1:
    currentTab = currentTab + n;
 
-   // if you have reached the end of the form... :
    if (currentTab >= x.length) 
    {
-      //...the form gets submitted:
       document.getElementById("register").submit();
       return false;
    }
    
-   // Otherwise, display the correct tab:
    showTab(currentTab);
 }
 
 function validateForm()  
 {
-   // This function deals with validation of the form fields
    let x, y, i, valid = true;
    x = document.getElementsByClassName("tab");
    y = x[currentTab].getElementsByTagName("input");
 
-   // A loop that checks every input field in the current tab:
    for (i = 0; i < y.length; i++) 
    {
-      // If a field is empty...
       if (y[i].value == "") 
       {
-         // add an "invalid" class to the field:
          y[i].className += " invalid";
-         // and set the current valid status to false:
          valid = false;
       }
    }
    
-   // If the valid status is true, mark the step as finished and valid:
    if (valid) 
    {
       document.getElementsByClassName("step")[currentTab].className += " finish";
 
    }
-   return valid; // return the valid status
+   return valid; 
 }
 
 function fixStepIndicator(n) 
